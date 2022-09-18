a3ffafa85cbc58413c3cddc7411468b10f07f253
justin <JustinCarpenter2020@Gmail.com>
2022-09-18 17:17:31 -0600

    base navbar dropdowns done

--- runcards.client/src/components/Navbar.vue
+++ runcards.client/src/components/Navbar.vue
@@ -1,10 +1,115 @@
 <template>
   <div class="bg-dark nav align-items-center justify-content-between">
-    <div class="col-4">
-      <h1 class="ms-2">DPatch</h1>
+    <div class="nav-item">
+  <button type="button" class="btn btn-secondary" data-bs-toggle="dropdown" aria-expanded="false">
+    Station 1
+  </button>
+  <div class="dropdown-menu">
+  <a class="dropdown-item">12345</a>
+  <a class="dropdown-item">12435</a>
+  <a class="dropdown-item">12534</a>
+  <a class="dropdown-item">13452</a>
+  <a class="dropdown-item">13524</a>
+  <a class="dropdown-item">14235</a>
+  <a class="dropdown-item">14235</a>
+  </div>
     </div>
+
+       <div class="nav-item">
+  <button type="button" class="btn btn-secondary" data-bs-toggle="dropdown" aria-expanded="false">
+    Station 2
+  </button>
+  <div class="dropdown-menu">
+     <a class="dropdown-item">21453</a>
+    <a class="dropdown-item">21543</a>
+     <a class="dropdown-item">24135</a>
+     <a class="dropdown-item">25134</a>
+  </div>
+    </div>
+
+           <div class="nav-item">
+  <button type="button" class="btn btn-secondary" data-bs-toggle="dropdown" aria-expanded="false">
+    Station 3
+  </button>
+  <div class="dropdown-menu">
+    <a class="dropdown-item">31452</a>
+    <a class="dropdown-item">31542</a>
+    <a class="dropdown-item">34152</a>
+    <a class="dropdown-item">35142</a>
+  </div>
+    </div>
+
+               <div class="nav-item">
+  <button type="button" class="btn btn-secondary" data-bs-toggle="dropdown" aria-expanded="false">
+    Station 4
+  </button>
+  <div class="dropdown-menu">
+ <a class="dropdown-item" routerLink="station/station4/41235">41235</a>
+ <a class="dropdown-item" routerLink="station/station4/41325">41325</a>
+  </div>
+    </div>
+
+                   <div class="nav-item">
+  <button type="button" class="btn btn-secondary" data-bs-toggle="dropdown" aria-expanded="false">
+    Station 5
+  </button>
+  <div class="dropdown-menu">
+    <a class="dropdown-item">51234</a>
+    <a class="dropdown-item">51324</a>
+    <a class="dropdown-item">52134</a>
+    <a class="dropdown-item">53124</a>
+  </div>
+    </div>
+
+                       <div class="nav-item">
+  <button type="button" class="btn btn-secondary" data-bs-toggle="dropdown" aria-expanded="false">
+    I-84
+  </button>
+  <div class="dropdown-menu">
+                              <a class="dropdown-item">13452</a>
+                              <a class="dropdown-item">31542</a>
+                              <a class="dropdown-item">34152</a>
+                              <a class="dropdown-item">35142</a>
+                              <a class="dropdown-item">43152</a>
+                              <a class="dropdown-item">53142</a>
+                              <a class="dropdown-item">43C/1</a>
+                              <a class="dropdown-item">C43/1</a>
+                              <a class="dropdown-item">M53/1</a>
+                              <a class="dropdown-item">53M/1</a>
+  </div>
+    </div>
+
+                           <div class="nav-item">
+  <button type="button" class="btn btn-secondary" data-bs-toggle="dropdown" aria-expanded="false">
+    HWY-55
+  </button>
+  <div class="dropdown-menu">
+    <a class="dropdown-item">C4/1</a>
+  </div>
+    </div>
+
+                               <div class="nav-item">
+  <button type="button" class="btn btn-secondary" data-bs-toggle="dropdown" aria-expanded="false">
+      Upper Deer Flat
+  </button>
+  <div class="dropdown-menu">
+    <a class="dropdown-item">21453</a>
+  </div>
+    </div>
+
+                                   <div class="nav-item">
+  <button type="button" class="btn btn-secondary" data-bs-toggle="dropdown" aria-expanded="false">
+      Wildlife Refuge
+  </button>
+  <div class="dropdown-menu">
+    <a class="dropdown-item">WR214</a>
+    <a class="dropdown-item">WR241</a>
+    <a class="dropdown-item">WR412</a>
+  </div>
+    </div>
+
     <!-- TODO needs to be user.isAuthenticated -->
-    <div class="col-4 text-end">
+    <div class="nav-item">
       <button class="btn btn-warning me-2" data-bs-toggle="offcanvas" data-bs-target="#information" aria-controls="information">Information</button>
     </div>
 
