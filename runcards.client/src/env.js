dbe006a84f0240eb1df49b03c2bb26929bcd23ea
justin <JustinCarpenter2020@Gmail.com>
2022-09-18 16:33:18 -0600

    added auth configuration in env.js

--- runcards.client/src/env.js
+++ runcards.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
-export const domain = ''
-export const clientId = ''
-export const audience = ''
+export const domain = 'dev-1nyr4hzf.us.auth0.com'
+export const clientId = 'j8fltcNr3LHq8LtCux9Zlzn37cw7nEZV'
+export const audience = 'http://NFDRunCards'
