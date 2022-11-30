2ef5205e373bedafdd9181c901ff34e004acb96b
JakeCarp <jakecarpenter407@gmail.com>
2022-11-30 11:33:37 -0700

    not sure what's in this but commiting to start

--- runcards.client/src/env.js
+++ runcards.client/src/env.js
@@ -4,3 +4,4 @@ export const useSockets = false
 export const domain = 'dev-1nyr4hzf.us.auth0.com'
 export const clientId = 'j8fltcNr3LHq8LtCux9Zlzn37cw7nEZV'
 export const audience = 'https://NFDRunCards'
+export const tinyApiKey = 'n6gh82c0uun6ro0q5fx5a67izbfkxon803oyi40kwq00078y'
---
+++ runcards.client/src/pages/RunCardPage.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="container ms-0 ps-0">
+        <div class="row justify-content-between">
+            <div class="col-3">
+                <div class="flex-shrink-0 p-3 bg-white" style="width: 280px;">
+                    <div class="d-flex align-items-center pb-3 mb-3 link-dark text-decoration-none border-bottom">
+                        <span class="fs-5 fw-semibold">Related Cards</span>
+                    </div>
+                    <ul class="list-unstyled ps-0">
+                       
+                    </ul>
+                </div>
+            </div>
+            <div class="card col-6">
+                <editor :api-key="tinyApiKey" :init="{
+    plugins: 'preview importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars emoticons',
+    menubar: 'file edit view insert format tools table help',
+    toolbar: 'undo redo | bold italic underline strikethrough | fontfamily fontsize blocks | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl'
+
+                }" />
+            </div>
+            <div class="col-3">
+
+            </div>
+        </div>
+    </div>
+
+</template>
+
+
+<script>
+import Editor from '@tinymce/tinymce-vue'
+import { computed, onMounted } from '@vue/runtime-core'
+import { router } from '../router'
+import { useRoute } from 'vue-router'
+import { runCardService } from '../services/RunCardService'
+import {tinyApiKey} from '../env'
+import { AppState } from '../AppState'
+
+export default {
+  components: { 'editor': Editor},
+    setup() {
+        const route = useRoute()
+        onMounted(() => {
+            // runCardService.getCardsInGroup(route.params.groupId);
+        })
+        const cards = computed(() => AppState.cards)
+        const currentCard = computed(() => AppState.currentCard)
+        return {
+            tinyApiKey,
+            cards,
+            currentCard
+        }
+    }}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
