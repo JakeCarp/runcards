d592c13fb9303da3fb1b3e1dcac960207ecd7c0f
justin <JustinCarpenter2020@Gmail.com>
2022-12-10 13:45:29 -0700

    feat: new auth audience

--- runcards.client/src/env.js
+++ runcards.client/src/env.js
@@ -3,4 +3,4 @@ export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
 export const domain = 'dev-elomoai1.us.auth0.com'
 export const clientId = 'WgugQOeyFsq16ZSPo5vUIIqrkfdTwQaa'
-export const audience = 'https://messenger'
+export const audience = 'https://runcards'
