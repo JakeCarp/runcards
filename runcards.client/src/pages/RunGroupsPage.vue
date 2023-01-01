59e133f7a4864aaae47a048a72b8e24e591b6fbc
justin <JustinCarpenter2020@Gmail.com>
2023-01-01 15:44:32 -0700

    adjusted page col

--- runcards.client/src/pages/RunGroupsPage.vue
+++ runcards.client/src/pages/RunGroupsPage.vue
@@ -1,17 +1,17 @@
 <template>
     <div class="container">
-        <div class="row">
-            <h1 class="col-12 text-center" v-if="selectedStation">Station: {{selectedStation}}</h1>
-            <h2 class="col-12 text-center" v-if="selectedZone">Zone: {{selectedZone}}</h2>
+        <div class="row justify-content center mt-3">
+            <h1 class="col-md-10 offset-md-2 text-center" v-if="selectedStation">Station: {{selectedStation}}</h1>
+            <h2 class="col-md-10 offset-md-2 text-center" v-if="selectedZone">Zone: {{selectedZone}}</h2>
         </div>
         <div v-if="selectedStation" class="row">
-            <h3 class="col-12 text-center">EMS</h3>
+            <h3 class="col-md-10 offset-md-2 text-center">EMS</h3>
         </div>
         <div v-if="selectedStation" class="row p-4 justify-content-between">
             <RunGroup  v-for="group in emsGroups" :key="group.id" :group="group"></RunGroup>
         </div>
         <div v-if="selectedStation" class="row">
-            <h3 class="col-12 text-center">Fire</h3>
+            <h3 class="col-md-10 offset-md-2  text-center">Fire</h3>
         </div>
         <div v-if="selectedStation" class="row p-4 justify-content-between">
             <RunGroup v-for="group in fireGroups" :key="group.id" :group="group"></RunGroup>
