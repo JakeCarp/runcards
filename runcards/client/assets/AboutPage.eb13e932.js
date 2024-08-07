42b6a807661058cca8713ed952ba4399b95addde
Jacob Carpenter <44380052+JakeCarp@users.noreply.github.com>
2024-08-07 12:15:54 -0600

    build

---
+++ runcards/client/assets/AboutPage.eb13e932.js
@@ -0,0 +1,1 @@
+import{_ as o,c as a,a as t,b as s,o as n,r as c}from"./index.e44876de.js";const r={name:"AboutPage"},_={class:"conatiner-fluid"},i=s("h1",null,"This is the about page",-1);function p(d,l,u,f,m,b){const e=c("Navbar");return n(),a("div",_,[t(e),i])}const v=o(r,[["render",p]]);export{v as default};
